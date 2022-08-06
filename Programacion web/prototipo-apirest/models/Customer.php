5a3dd0e7a6520d937a66bc743cbec47f08e56f0c
german <german.estefan81@gmail.com>
2022-08-06 19:35:38 -0300

    Flujo del login realizado(Falta generar token y enviarlo al cliente)

--- Programacion web/prototipo-apirest/models/Customer.php
+++ Programacion web/prototipo-apirest/models/Customer.php
@@ -6,8 +6,8 @@
         private $company;
         private $nRut;  
 
-        function __construct($mail, $name, $surname, $phone, $password, $address, $typeCustomer, $company, $nRut){
-            parent::__construct($mail, $name, $surname, $phone, $password, $address);
+        function __construct($email, $name, $surname, $phone, $password, $address, $typeCustomer, $company, $nRut){
+            parent::__construct($email, $name, $surname, $phone, $password, $address);
             $this->typeCustomer = $typeCustomer;
             $this->company = $company;
             $this->nRut = $nRut;
@@ -15,8 +15,8 @@
         
         public function save(){
             //Hay que aplicar una TRANSACCION(PENDIENTE)
-            $query1 = "INSERT INTO usuario(mail, nombre, apellido, telefono, direccion, password) VALUES ('$this->mail', '$this->name', '$this->surname', '$this->phone', '$this->address', '$this->password' )";
-            $query2 = "INSERT INTO cliente(mail, tipocli, empresa, nrut) VALUES ('$this->mail', '$this->typeCustomer', '$this->company', '$this->nRut')";
+            $query1 = "INSERT INTO user(email, name, surname, phone, address, password) VALUES ('$this->email', '$this->name', '$this->surname', '$this->phone', '$this->address', '$this->password' )";
+            $query2 = "INSERT INTO customer(email, type_customer, company, rut) VALUES ('$this->mail', '$this->typeCustomer', '$this->company', '$this->nRut')";
             //Si devuelve 11 es por que ambas se ejecutaron con exito
             echo $this->connection->setData($query1);
             echo $this->connection->setData($query2);
--- Programacion web/prototipo-apirest/models/User.php
+++ Programacion web/prototipo-apirest/models/User.php
@@ -1,7 +1,7 @@
 <?php
     class User {
 
-        protected $mail;
+        protected $email;
         protected $name;
         protected $surname;
         protected $phone;
@@ -9,14 +9,20 @@
         protected $address;
         protected $connection;
 
-        function __construct($mail, $name, $surname, $phone, $password, $address){
-            $this->mail = $mail;
+        function __construct($email, $name, $surname, $phone, $password, $address){
+            $this->email = $email;
             $this->name = $name;
             $this->surname = $surname;
             $this->password = $password;
             $this->phone = $phone;
             $this->address = $address;
             $this->connection = new Connection();
+        }
+
+        public static function getUserByEmail($email){
+            $conecction = new Connection();
+            $query = "SELECT * from user WHERE email='$email'";
+            return $conecction->getData($query)->fetch_assoc();
         }
     }
         
