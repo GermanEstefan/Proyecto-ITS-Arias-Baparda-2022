e59395917a32d51941cf0fd398e4db80c1b2a09a
Alvarez Resende <e685863@net.in.iantel.com.uy>
2022-08-15 16:50:55 -0300

    correccion en STG

--- Base de datos/SEGUNDA ENTREGA/ambiente STG/bindevSTG.sql
+++ Base de datos/SEGUNDA ENTREGA/ambiente STG/bindevSTG.sql
@@ -4,15 +4,15 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 -- -----------------------------------------------------
--- Schema bindevSTG
+-- Schema bindevstg
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `bindevSTG` DEFAULT CHARACTER SET utf8 ;
-USE `bindevSTG` ;
+CREATE SCHEMA IF NOT EXISTS `bindevstg` DEFAULT CHARACTER SET utf8 ;
+USE `bindevstg` ;
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`category`
+-- Table `bindevstg`.`category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`category` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`category` (
   `id_category` INT NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`category` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`desing`
+-- Table `bindevstg`.`desing`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`desing` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`desing` (
   `id_desing` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -33,9 +33,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`size`
+-- Table `bindevstg`.`size`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`size` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`size` (
   `id_size` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -45,9 +45,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`disburse`
+-- Table `bindevstg`.`disburse`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`disburse` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`disburse` (
   `id_disburse` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -56,9 +56,9 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`disburse` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`supplier`
+-- Table `bindevstg`.`supplier`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`supplier` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`supplier` (
   `id_supplier` INT NOT NULL,
   `rut` varchar(12) NOT NULL,
   `company_name` VARCHAR(150) NOT NULL,
@@ -70,9 +70,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`role`
+-- Table `bindevstg`.`role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`role` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`role` (
   `name_role` VARCHAR(100) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`name_role`))
@@ -80,9 +80,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`users`
+-- Table `bindevstg`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`users` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`users` (
   `id_user` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(200) NOT NULL,
   `name` VARCHAR(150) NOT NULL,
@@ -95,9 +95,9 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`users` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`status`
+-- Table `bindevstg`.`status`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`status` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`status` (
   `id_status` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -107,9 +107,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`delivery_time`
+-- Table `bindevstg`.`delivery_time`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`delivery_time` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`delivery_time` (
   `id_delivery` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -118,9 +118,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`payment_method`
+-- Table `bindevstg`.`payment_method`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`payment_method` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`payment_method` (
   `id_pay_meth` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -130,9 +130,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`photos`
+-- Table `bindevstg`.`photos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`photos` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`photos` (
   `id_photo` INT NOT NULL,
   `name` VARCHAR(150) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -141,9 +141,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`discount`
+-- Table `bindevstg`.`discount`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`discount` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`discount` (
   `value` FLOAT NOT NULL,
   `description` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`value`))
@@ -151,9 +151,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`employee`
+-- Table `bindevstg`.`employee`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`employee` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`employee` (
   `ci` INT NOT NULL,
   `employee_user` INT NOT NULL,
   `employee_role` VARCHAR(100) NOT NULL,
@@ -161,21 +161,21 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`employee` (
   constraint `UN_ci` unique (`ci`),
   CONSTRAINT `FK_employee_role`
     FOREIGN KEY (`employee_role`)
-    REFERENCES `bindevSTG`.`role` (`name_role`)
+    REFERENCES `bindevstg`.`role` (`name_role`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `FK_employee_user`
     FOREIGN KEY (`employee_user`)
-    REFERENCES `bindevSTG`.`users` (`id_user`)
+    REFERENCES `bindevstg`.`users` (`id_user`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`client`
+-- Table `bindevstg`.`client`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`client` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`client` (
   `client_user` INT NOT NULL,
   `company_name` VARCHAR(300) NULL,
   `rut_nr` varchar(12) NULL,
@@ -183,16 +183,16 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`client` (
   constraint `UN_rut_company` UNIQUE  (`rut_nr`, `company_name`),
   CONSTRAINT `FK_user_id`
     FOREIGN KEY (`client_user`)
-    REFERENCES `bindevSTG`.`users` (`id_user`)
+    REFERENCES `bindevstg`.`users` (`id_user`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`supply`
+-- Table `bindevstg`.`supply`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`supply` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`supply` (
   `id_supply` INT NOT NULL AUTO_INCREMENT ,
   `date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ,
   `supplier_id` INT NOT NULL,
@@ -202,26 +202,26 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`supply` (
   PRIMARY KEY (`id_supply`),
   CONSTRAINT `FK_supply_supplier`
     FOREIGN KEY (`supplier_id`)
-    REFERENCES `bindevSTG`.`supplier` (`id_supplier`)
+    REFERENCES `bindevstg`.`supplier` (`id_supplier`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `FK_supply_employee`
     FOREIGN KEY (`employee_ci`)
-    REFERENCES `bindevSTG`.`employee` (`ci`)
+    REFERENCES `bindevstg`.`employee` (`ci`)
     ON DELETE RESTRICT
     ON UPDATE cascade,
   CONSTRAINT `FK_supply_disburse`
     FOREIGN KEY (`disburse_method`)
-    REFERENCES `bindevSTG`.`disburse` (`id_disburse`)
+    REFERENCES `bindevstg`.`disburse` (`id_disburse`)
     ON DELETE RESTRICT
     ON UPDATE cascade)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`product`
+-- Table `bindevstg`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`product` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`product` (
   `barcode` INT NOT NULL AUTO_INCREMENT,
   `id_product` INT NOT NULL,
   `name` VARCHAR(200) NOT NULL,
@@ -235,46 +235,46 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`product` (
   constraint `UN_product` UNIQUE (`id_product`, `product_category`, `product_desing`, `product_size`),
   CONSTRAINT `FK_category_product`
     FOREIGN KEY (`product_category`)
-    REFERENCES `bindevSTG`.`category` (`id_category`)
+    REFERENCES `bindevstg`.`category` (`id_category`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `FK_desing_product`
     FOREIGN KEY (`product_desing`)
-    REFERENCES `bindevSTG`.`desing` (`id_desing`)
+    REFERENCES `bindevstg`.`desing` (`id_desing`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `FK_size_product`
     FOREIGN KEY (`product_size`)
-    REFERENCES `bindevSTG`.`size` (`id_size`)
+    REFERENCES `bindevstg`.`size` (`id_size`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`galery`
+-- Table `bindevstg`.`galery`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`galery` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`galery` (
   `product_galery` INT NOT NULL,
   `photo_galery` INT NOT NULL,
   PRIMARY KEY (`product_galery`, `photo_galery`),
 	CONSTRAINT `FK_product_galery`
     FOREIGN KEY (`product_galery`)
-    REFERENCES `bindevSTG`.`product` (`barcode`)
+    REFERENCES `bindevstg`.`product` (`barcode`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `FK_photo_product`
     FOREIGN KEY (`photo_galery`)
-    REFERENCES `bindevSTG`.`photos` (`id_photo`)
+    REFERENCES `bindevstg`.`photos` (`id_photo`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`supply_detail`
+-- Table `bindevstg`.`supply_detail`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`supply_detail` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`supply_detail` (
   `supply_id` INT NOT NULL,
   `barcode_id` INT NOT NULL,
   `quantity` INT NOT NULL,
@@ -283,21 +283,21 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`supply_detail` (
   PRIMARY KEY (`supply_id`, `barcode_id`),
 CONSTRAINT `FK_product_reference`
     FOREIGN KEY (`barcode_id`)
-    REFERENCES `bindevSTG`.`product` (`barcode`)
+    REFERENCES `bindevstg`.`product` (`barcode`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `FK_supply_reference`
     FOREIGN KEY (`supply_id`)
-    REFERENCES `bindevSTG`.`supply` (`id_supply`)
+    REFERENCES `bindevstg`.`supply` (`id_supply`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`sale`
+-- Table `bindevstg`.`sale`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`sale` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`sale` (
   `id_sale` INT NOT NULL AUTO_INCREMENT,
   `date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ,
   `address` VARCHAR(500) NOT NULL,
@@ -307,26 +307,26 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`sale` (
   PRIMARY KEY (`id_sale`),
   CONSTRAINT `FK_client_user`
     FOREIGN KEY (`user_purchase`)
-    REFERENCES `bindevSTG`.`client` (`client_user`)
+    REFERENCES `bindevstg`.`client` (`client_user`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `FK_delivery_sale`
     FOREIGN KEY (`sale_delivery`)
-    REFERENCES `bindevSTG`.`delivery_time` (`id_delivery`)
+    REFERENCES `bindevstg`.`delivery_time` (`id_delivery`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `FK_pay_sale`
     FOREIGN KEY (`pay_met`)
-    REFERENCES `bindevSTG`.`payment_method` (`id_pay_meth`)
+    REFERENCES `bindevstg`.`payment_method` (`id_pay_meth`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`report`
+-- Table `bindevstg`.`report`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`report` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`report` (
   `sale_report` INT NOT NULL,
   `status_report` INT NOT NULL,
   `employee_report` INT NOT NULL,
@@ -336,26 +336,26 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`report` (
   CONSTRAINT `UN_report_sale` UNIQUE (`sale_report`, `status_report`, `date`),
   CONSTRAINT `FK_sale_report`
     FOREIGN KEY (`sale_report`)
-    REFERENCES `bindevSTG`.`sale` (`id_sale`)
+    REFERENCES `bindevstg`.`sale` (`id_sale`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `FK_status_report`
     FOREIGN KEY (`status_report`)
-    REFERENCES `bindevSTG`.`status` (`id_status`)
+    REFERENCES `bindevstg`.`status` (`id_status`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `FK_employye_report`
     FOREIGN KEY (`employee_report`)
-    REFERENCES `bindevSTG`.`employee` (`ci`)
+    REFERENCES `bindevstg`.`employee` (`ci`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`sale_detail`
+-- Table `bindevstg`.`sale_detail`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`sale_detail` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`sale_detail` (
   `sale_id` INT NOT NULL,
   `product_sale` INT NOT NULL,
   `quantity` INT NOT NULL,
@@ -365,38 +365,38 @@ CREATE TABLE IF NOT EXISTS `bindevSTG`.`sale_detail` (
   PRIMARY KEY (`sale_id`, `product_sale`),
   CONSTRAINT `FK_id_sale`
     FOREIGN KEY (`sale_id`)
-    REFERENCES `bindevSTG`.`sale` (`id_sale`)
+    REFERENCES `bindevstg`.`sale` (`id_sale`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `FK_prod_sale`
     FOREIGN KEY (`product_sale`)
-    REFERENCES `bindevSTG`.`product` (`barcode`)
+    REFERENCES `bindevstg`.`product` (`barcode`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `FK_value_discount`
     FOREIGN KEY (`sale_discount`)
-    REFERENCES `bindevSTG`.`discount` (`value`)
+    REFERENCES `bindevstg`.`discount` (`value`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bindevSTG`.`promo`
+-- Table `bindevstg`.`promo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bindevSTG`.`promo` (
+CREATE TABLE IF NOT EXISTS `bindevstg`.`promo` (
   `is_product` INT NOT NULL,
   `have_product` INT NOT NULL,
   `quantity` INT NOT NULL,
   PRIMARY KEY (`is_product`, `have_product`),
   CONSTRAINT `FK_is_product`
     FOREIGN KEY (`is_product`)
-    REFERENCES `bindevSTG`.`product` (`barcode`)
+    REFERENCES `bindevstg`.`product` (`barcode`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `FK_have_product`
     FOREIGN KEY (`have_product`)
-    REFERENCES `bindevSTG`.`product` (`barcode`)
+    REFERENCES `bindevstg`.`product` (`barcode`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
