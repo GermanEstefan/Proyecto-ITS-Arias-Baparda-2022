4bded781de242ae2181042b9009d22863663912b
Fabricio Rivera <Fabricio.rivera2012@gmail.com>
2022-08-17 08:48:31 -0300

    Menu de adnin de base de datos actualizado

--- Sistemas operativos/DataBaseAdmin.sh
+++ Sistemas operativos/DataBaseAdmin.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-
+#
+#variables
+DATABASE="tareaSO"
+USER="admin"
+PASSWORD="1234"
+RESPALDOS="/home/fabricio/respaldos"
+DAILY="@daily"
+WEEKLY="@weekly"
+CUSTOMCRON=""
+#
+#
 #Menu principal
 opt=1
+opt2=1
 while [ "$opt" != 0 ]
 	do
 	echo "Bienvenido al sistema de administracion de la base de datos"
@@ -16,59 +27,74 @@ while [ "$opt" != 0 ]
 	case $opt in
 		1)
 		clear
-		if [ -n "$DATABASE" ]
-		then
-			echo "Actualmente hay una base de datos seleccionada"
-			echo "BD: "$DATABASE
-			echo "--------------------------------------------------"
-			echo "Desea continuar con esta base de datos Y/n"
-			read answer
-			if [ $answer = "Y" ] | [ $answer = "y" ]
-			then
-				mysqldump -u admin -p1234 $DATABASE >> /home/fabricio/respaldos/$DATABASE".sql"
-				tar -zcvf /home/fabricio/respaldos/$(date +%d-%m-%y)_respaldo.tar.gz /home/fabricio/respaldos/$DATABASE".sql"
-				rm /home/fabricio/respaldos/$DATABASE".sql"
-				echo "Backup realizado en /home/fabricio/respaldos/"
-				ls /home/fabricio/respaldos/
-				sleep 2s
-			else
-				echo "--------------------------------------"
-				echo "Escriba el nombre de la base de datos:"
-				read db
-				export DATABASE=$db
-				if [ -e /home/fabricio/respaldos/database.txt ]
-				then
-					$DATABASE":" >> /home/fabricio/respaldos/database.txt
-				else
-					$DATABASE":" > /home/fabricio/respaldos/database.txt
-				fi
-			fi
-		else
-			echo "No hay base de datos seleccionada"
-			echo "---------------------------------------"
-			echo "Bases de datos disponibles"
-			if [ -e /home/fabricio/respaldos/database.txt ]
-			then
-				cat /home/fabricio/respaldos/database.txt | cut -d":" -f 1
-			else
-				echo "No hay bases de datos guardadas"
-			fi
-			echo "Escriba el nombre de la base de datos a utilizar:"
-			read db
-			export DATABASE=$db
-			echo $DATABASE":" > /home/fabricio/respaldos/database.txt
-		fi
+		echo "Base de datos: "$DATABASE
+		echo "--------------------------------------------------"
+		mysqldump -u $USER -p$PASSWORD $DATABASE >> $RESPALDOS/$DATABASE".sql"
+		tar -zcvf $RESPALDOS/$(date +%d-%m-%y)_respaldo.tar.gz $RESPALDOS/$DATABASE".sql"
+		rm $RESPALDOS/$DATABASE".sql"
+		echo "Backup realizado en /home/fabricio/scripts/"
+		ls $RESPALDOS/
+		sleep 2s
 		;;
 		2)
 		clear
-		tar -zxf respaldo.tar.gz
-		mysql -u admin -p1234 tareaSO < tareaSO".sql"
-		rm tareaSO.sql
-		echo "Importación realizada"
+		echo "Registro de respaldos"
+		ls -1hr $RESPALDOS/
+		echo "Seleccione un respaldo de la lista"
+		read resp
+		if [ -d $RESPALDOS/$resp ]
+		then
+			tar -zxf $RESPALDO/respaldo.tar.gz
+			mysql -u $USER -p$PASSWORD $DATABASE < $DATABASE".sql"
+			rm tareaSO.sql
+			echo "Importación realizada"
+			sleep 1s
+		else
+			echo "El archivo que quieres importar no existe"
+			echo "asegurate de que el nombre este bien escrito"
+			echo "--------------------------------------------"
+			sleep 1s
+		fi
 		;;
 		3)
-		clear 
-		echo "Caso 3"
+		clear
+		while [ "$opt2" != 0 ]
+		do 
+		echo "Configuración por defecto:"
+		echo "1) Ruta de los respaldos: "$RESPALDOS/
+		echo "2) Asiduidad de los respaldos: "$DAILY
+		echo "3) Ruta del servidor de respaldo: "$BACKUPSERVER
+		echo "0) Volver al menu principal"
+		echo "---------------------------------------------------"
+		echo "SELECCIONE LA CONFIGURACION QUE DESEA CAMBIAR"
+		read opt2
+		case $opt2 in
+			1)
+			echo "Escriba el nombre de la ruta donde esta la carpeta de respaldos"
+			read ruta
+			if [ -d $ruta ]
+			then
+				sed -i s%$RESPALDOS%$ruta%g /home/fabricio/scripts/DataBaseAdminPrueba.sh
+				echo "La nueva ruta es: "$ruta
+				sleep 2s
+			else
+				echo "La ruta que selecciono no es valida"
+				sleep 1s
+			fi
+			;;
+			2)
+			;;
+			3)
+			;;
+			0)
+			echo "Menu Principal"
+			;;
+			*)
+			echo "Seleccione una opcion valida"
+			sleep 1s
+			;;
+		esac
+		done
 		;;
 		4)
 		clear
@@ -83,4 +109,3 @@ while [ "$opt" != 0 ]
 		;;
 		esac
 	done
-
