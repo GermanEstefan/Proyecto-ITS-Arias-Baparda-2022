a965c7b3e650eff73167e4b56f7e77aa8df1554b
Alvarez Nicolas <formacionalvareznicolas@gmail.com>
2022-08-20 14:15:47 -0300

    correccion update-galery

--- Base de datos/SEGUNDA ENTREGA/ambiente STG/update/update-galery.sql
+++ Base de datos/SEGUNDA ENTREGA/ambiente STG/update/update-galery.sql
@@ -2,16 +2,6 @@
 --CAMBIAR FOTOS DE UN PRODUCTO
 update galery
 set photo_galery = 'new photo'
-where product_galery = '12312300'; 
---FIN
-
----------------------------------------------------DELETE GALERY
---QUITAR RELACION DE PRODUCTO CON FOTOS (NO BORRA LA FOTO DE LA BD)
-delete galery
-where product_galery = '12312300'; 
---FIN
-
---QUITAR UNA FOTO DEL PRODUCTO (NO BORRA LA FOTO DE LA BD)
-delete galery
 where product_galery = '12312300' and photo_galery = '911'; 
 --FIN
+
