1d5987d6163d330e4cadefbd12ade3181ea15193
german <german.estefan81@gmail.com>
2022-08-25 21:30:35 -0300

    Resubiendo backend

---
+++ Programacion web/api/auth-customers.php
@@ -0,0 +1,38 @@
+<?php
+include('./helpers/Response.php');
+include('./controllers/CustomerController.php');
+
+header('Content-Type: application/json'); //Le decimos al agente que consuma el servidor que vamos a devolver JSON.
+header('Access-Control-Allow-Origin: *'); //CORS
+
+$response = new Response(); 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if( !isset($_GET['url']) ){
+        echo $response->error400();
+        die();
+    }
+    $url = $_GET['url']; //Este valor sirve para diferenciar la accion(login, registro, etc). 
+
+    $bodyOfRequest = file_get_contents('php://input'); //Obtiene el body de la request sin procesar(JSON).
+    $userData = json_decode($bodyOfRequest, 1); //Transforma el JSON en un array asosciativo.
+    $customerController = new CustomerController(); 
+
+    switch ($url) {
+        case 'login':
+            $customerController->loginCustomer($userData);
+            die();
+        case 'register':
+            $customerController->registerCustomer($userData);
+            die();
+        default:
+            http_response_code(400);
+            echo $response->error400("Parametro URL invalido");
+            die();
+    }
+
+} else {
+    //Metodo no permitido.
+    echo $response->error405();
+    die();
+}
