3359226355220310fc5d80c0b7cb5ba4a6fbcd69
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-08-27 21:30:17 -0300

    Validaciones basicas en register

--- Diseño Web/Prototipo producto react/prototipo-producto/src/pages/Register.jsx
+++ Diseño Web/Prototipo producto react/prototipo-producto/src/pages/Register.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 import Imagen from "./../img/Obreros.jpg";
 import { Animated } from "react-animated-css";
 import { useForm } from "../hooks/useForm";
 import { URL } from "../API/URL";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 const Register = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
@@ -23,11 +25,26 @@ const Register = () => {
     company: "",
     nRut: "",
   };
-  const [values, handleValuesChange, resetForm] = useForm(initialValues);
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const [values, handleValuesChange] = useForm(initialValues);
+
+  const validate = (valuesParam) => {
+    const errors = {};
+    if (!valuesParam.name) errors.name = "El nombre es requerido";
+    if (!valuesParam.surname) errors.surname = "El apellido es requerido";
+    if (!valuesParam.email) errors.email = "El mail es requerido";
+    if (!valuesParam.password) errors.password = "La contraseña es requerido";
+    setFormErrors(errors);
+    return errors;
+  };
+
+  useEffect(() => {}, [values]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(JSON.stringify(values))
+    validate(values);
     const endpoint = URL + "auth-customers.php?url=register";
     fetch(endpoint, {
       method: "POST",
@@ -35,21 +52,21 @@ const Register = () => {
     })
       .then((resp) => resp.json())
       .then((respToJson) => {
-        console.log(respToJson)
-        
+        console.log(respToJson);
+        localStorage.setItem("token", "");
         Swal.fire({
-          icon: 'success',
-          text: 'Te registraste exitosamente',
+          icon: "success",
+          text: "Te registraste exitosamente",
           timer: 1000,
           showConfirmButton: false,
-        })
+        });
         setTimeout(() => {
-          navigate('/')
+          navigate("/");
         }, 1000);
-      }).catch((error) => {
-        console.error(error)
       })
-      resetForm()
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
@@ -62,46 +79,71 @@ const Register = () => {
           animationInDuration={500}
           isVisible={true}
         >
-          <form className="form" onSubmit={handleSubmit}>
+          <div className="form">
             <h1>Registrate para comenzar tu experiencia</h1>
-            <div>
-              <input
-                name="name"
-                id="name"
-                value={values.name}
-                placeholder="Nombre"
-                onChange={handleValuesChange}
-              ></input>
-            </div>
-            <div>
-              <input
-                name="surname"
-                id="surname"
-                value={values.surname}
-                placeholder="Apellido"
-                onChange={handleValuesChange}
-              ></input>
-            </div>
-            <div>
-              <input
-                name="email"
-                id="email"
-                value={values.email}
-                placeholder="Email"
-                onChange={handleValuesChange}
-              ></input>
-            </div>
-            <div>
-              <input
-                name="password"
-                id="password"
-                type={'password'}
-                value={values.password}
-                placeholder="Contraseña"
-                onChange={handleValuesChange}
-              ></input>
-            </div>
-            {/* <div>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input
+                  name="name"
+                  id="name"
+                  value={values.name}
+                  placeholder="Nombre"
+                  onChange={handleValuesChange}
+                ></input>
+                {formErrors.name && (
+                  <p className="formAlert">
+                    {formErrors.name}{" "}
+                    <FontAwesomeIcon icon={faCircleExclamation} />
+                  </p>
+                )}
+              </div>
+              <div>
+                <input
+                  name="surname"
+                  id="surname"
+                  value={values.surname}
+                  placeholder="Apellido"
+                  onChange={handleValuesChange}
+                ></input>
+                {formErrors.surname && (
+                  <p className="formAlert">
+                    {formErrors.surname}{" "}
+                    <FontAwesomeIcon icon={faCircleExclamation} />
+                  </p>
+                )}
+              </div>
+              <div>
+                <input
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  placeholder="Email"
+                  onChange={handleValuesChange}
+                ></input>
+                {formErrors.email && (
+                  <p className="formAlert">
+                    {formErrors.email}{" "}
+                    <FontAwesomeIcon icon={faCircleExclamation} />
+                  </p>
+                )}
+              </div>
+              <div>
+                <input
+                  name="password"
+                  id="password"
+                  type={"password"}
+                  value={values.password}
+                  placeholder="Contraseña"
+                  onChange={handleValuesChange}
+                ></input>
+                {formErrors.password && (
+                  <p className="formAlert">
+                    {formErrors.password}{" "}
+                    <FontAwesomeIcon icon={faCircleExclamation} />
+                  </p>
+                )}
+              </div>
+              {/* <div>
               <input
                 name="cpassword"
                 id="cpassword"
@@ -110,12 +152,15 @@ const Register = () => {
                 onChange={(e) => setPassword2(e.target.value)}
               ></input>
             </div> */}
-            <button className="submitButton" type="submit">Registrarse</button>
-            <br />
-            <Link className="link" to={"/login"}>
-              Ingresar
-            </Link>
-          </form>
+              <button className="submitButton" type="submit">
+                Registrarse
+              </button>
+              <br />
+              <Link className="link" to={"/login"}>
+                Ingresar
+              </Link>
+            </form>
+          </div>
         </Animated>
       </div>
     </>
