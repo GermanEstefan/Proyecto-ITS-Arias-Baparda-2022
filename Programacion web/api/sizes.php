7e0a80ff2eb54391791f8e0bfdcccad409e2646c
Alvarez Nicolas <formacionalvareznicolas@gmail.com>
2022-10-08 18:40:21 -0300

    method Delete en size.php

--- Programacion web/api/sizes.php
+++ Programacion web/api/sizes.php
@@ -25,6 +25,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     $size->getSizes();
 
+}else if($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+
+    if(!isset($_GET['idSize'])){
+        //Si manda este parametro seignifica que quiere obtener una categoria en particular
+        echo $response->error200("Error falta Id");    
+        die();
+    }
+    $idSize = $_GET['idSize'];
+    $size->deleteSize($idSize);
+    
 }else if($_SERVER['REQUEST_METHOD'] === 'PUT'){
     //Editar talle
     if(!isset($_GET['idSize'])){
