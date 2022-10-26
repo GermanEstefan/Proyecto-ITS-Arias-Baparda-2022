3c6cdd2c764642b9e652166b2ee3fa497568b93c
IgnacioArmandUgon <Matias4371@gmail.com>
2022-10-26 20:43:47 -0300

    Historial de compra maquetado y responsive

--- Diseño Web/Prototipo producto react/prototipo-producto/src/components/store/SalesHistory.jsx
+++ Diseño Web/Prototipo producto react/prototipo-producto/src/components/store/SalesHistory.jsx
@@ -1,20 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 import { fetchApi } from "../../API/api";
 import { userStatusContext } from "../../App";
+import { HistoryItem } from "./HistoryItem";
 
 const SalesHistory = () => {
   const { userData } = useContext(userStatusContext);
+  const [sales, setSales] = useState([]);
   useEffect(() => {
     getSalesHistory();
   }, []);
 
   const getSalesHistory = async () => {
-    console.log(`salesClient=${userData.email}`);
-    const resp = await fetchApi(`salesClient=${userData.email}`, "GET");
-    console.log(resp)
+    const resp = await fetchApi(
+      `sales.php?salesClient=${userData.email}`,
+      "GET"
+    );
+    resp && setSales(resp.result.data.sales);
   };
-  return <h1>Historial de compras</h1>;
+
+  return (
+    <>
+      <h1 style={{marginLeft: '15px'}}>Historial de compras</h1>
+      {sales.map((sale, index) => (
+        <HistoryItem key={index} sale={sale} />
+      ))}
+    </>
+  );
 };
 
 export default SalesHistory;
