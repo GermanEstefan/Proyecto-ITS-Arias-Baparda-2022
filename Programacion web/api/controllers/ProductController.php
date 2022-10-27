f36bfca7cf64cc0fae811b33cbfaccd289bb71ae
Alvarez Nicolas <formacionalvareznicolas@gmail.com>
2022-10-27 11:03:11 -0300

    dos comillas

--- Programacion web/api/controllers/ProductController.php
+++ Programacion web/api/controllers/ProductController.php
@@ -146,7 +146,7 @@ class ProductController
                 echo $this->response->error203("Esta intentando ingresar un producto ya existente");
                 die();
             }
-            $query = array($index => "INSERT INTO product (id_product, name, product_category, product_design, product_size, stock, price, description, picture) VALUES ('$idProduct','$name','$prodCategory','$prodDesign','$prodSize','$stock','$price', '$description', $picture)");
+            $query = array($index => "INSERT INTO product (id_product, name, product_category, product_design, product_size, stock, price, description, picture) VALUES ('$idProduct','$name','$prodCategory','$prodDesign','$prodSize','$stock','$price', '$description', '$picture')");
             array_push($queries, $query);
             $index++;
         }
