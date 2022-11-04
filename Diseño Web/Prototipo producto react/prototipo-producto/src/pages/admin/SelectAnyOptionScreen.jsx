a1948001120b2337f186e1564f5613d95e7e15b0
IgnacioArmandUgon <Matias4371@gmail.com>
2022-11-03 22:43:12 -0300

    conflicto

--- Diseño Web/Prototipo producto react/prototipo-producto/src/pages/admin/SelectAnyOptionScreen.jsx
+++ Diseño Web/Prototipo producto react/prototipo-producto/src/pages/admin/SelectAnyOptionScreen.jsx
@@ -1,3 +1,4 @@
+/** @format */
 
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,22 +7,21 @@ import { userStatusContext } from "../../App";
 import { capitalizeString } from "../../helpers/capitalizeString";
 
 const SelectAnyOptionScreen = () => {
+  const { userData } = useContext(userStatusContext);
+  const { name, surname } = userData;
 
-    const { userData } = useContext(userStatusContext);
-    const { name, surname} = userData;
+  return (
+    <section className="container_section select-any-opt flex-column-center-xy">
+      <div className="flex-column-center-xy">
+        <h1>Bienvenido :</h1>
 
-    return(
-        <section className="container_section select-any-opt flex-column-center-xy">
-            <div className="flex-column-center-xy">
-                <h1>Bienvenido :</h1> 
-                <h2>{capitalizeString(`${name} ${surname}`)}</h2>
-                <p>Seleccione alguna opción del menú para comenzar la gestión</p>
-                <h2>{ (name && surname) && capitalizeString(`${name} ${surname}`)}</h2>
-                <p>Seleccione alguna opcion del menu para comenzar la gestion</p>
-                <FontAwesomeIcon icon={faStar} />
-            </div>
-        </section>
-    )
-}
+        <h2>{name && surname && capitalizeString(`${name} ${surname}`)}</h2>
+        <p>Seleccione alguna opción del menú para comenzar la gestión</p>
+
+        <FontAwesomeIcon icon={faStar} />
+      </div>
+    </section>
+  );
+};
 
 export default SelectAnyOptionScreen;
