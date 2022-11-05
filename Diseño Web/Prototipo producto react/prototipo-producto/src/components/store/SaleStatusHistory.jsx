de06f04e4f43c52ba7c555d20001c6d041ed2d39
IgnacioArmandUgon <Matias4371@gmail.com>
2022-11-05 14:14:30 -0300

    Ajuste en historial de estados de compra

--- Diseño Web/Prototipo producto react/prototipo-producto/src/components/store/SaleStatusHistory.jsx
+++ Diseño Web/Prototipo producto react/prototipo-producto/src/components/store/SaleStatusHistory.jsx
@@ -25,7 +25,7 @@ const SaleStatusHistory = ({ saleId }) => {
   const getStatuslist = async () => {
     const resp = await fetchApi(`sales.php?History=${saleId}`, "GET");
     console.log(resp);
-    setLastStatus(resp.result.data.history[resp.result.data.history.length - 1].status || "");
+    setLastStatus(resp.result.data.history[0].status || "");
   };
   return (
     <div className="statusHistory">
