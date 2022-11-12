c71aae5fc2cd2b1f6c5aa8144c55dc472d753b73
German <german.estefan81@gmail.com>
2022-11-12 13:46:28 -0300

    build subido

---
+++ Diseño Web/Front-end Producto React/build/precache-manifest.eabf1b8b2bbc19540d5f7cca22555161.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "79423137ccb7690a6852",
+    "url": "./static/css/main.2cb143aa.chunk.css"
+  },
+  {
+    "revision": "79423137ccb7690a6852",
+    "url": "./static/js/main.79423137.chunk.js"
+  },
+  {
+    "revision": "3b850aaac13298c8ffb5",
+    "url": "./static/css/1.6e0bc268.chunk.css"
+  },
+  {
+    "revision": "3b850aaac13298c8ffb5",
+    "url": "./static/js/1.3b850aaa.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "210272344c90c2d6545f192936eb7b4f",
+    "url": "./static/media/no-photo.21027234.png"
+  },
+  {
+    "revision": "b5ef9172434f38c840baa360b9dd46bc",
+    "url": "./static/media/Obreros.b5ef9172.jpg"
+  },
+  {
+    "revision": "99ec86777da6add97753601fbd033612",
+    "url": "./static/media/Cliente-nombre1.99ec8677.svg"
+  },
+  {
+    "revision": "ddf20667433395b86225781fe8ea2d0b",
+    "url": "./static/media/loading.ddf20667.gif"
+  },
+  {
+    "revision": "41098c2e7d23adfce00941dd35b9813c",
+    "url": "./static/media/emptybox.41098c2e.png"
+  },
+  {
+    "revision": "11c978a8e344c92ee25c378f493dfbd5",
+    "url": "./static/media/facebook-brands.11c978a8.svg"
+  },
+  {
+    "revision": "ab1ab6581eaca357c36eaf169bf44ebb",
+    "url": "./static/media/instagram-brands.ab1ab658.svg"
+  },
+  {
+    "revision": "e4d6fc95a7634a5cd1eee08558e45db4",
+    "url": "./static/media/message-solid.e4d6fc95.svg"
+  },
+  {
+    "revision": "f7c786640154634d064c500dd3db304e",
+    "url": "./static/media/whatsapp.f7c78664.svg"
+  },
+  {
+    "revision": "c79e54ccd04fcb995f59e46ecdea3ad1",
+    "url": "./static/media/Cliente-logo1.c79e54cc.svg"
+  },
+  {
+    "revision": "4fa004451a008151b8e5f04321838218",
+    "url": "./static/media/purchase.4fa00445.jpg"
+  },
+  {
+    "revision": "621654cdb168aa31fb356da7825c4881",
+    "url": "./static/media/Banner.621654cd.jpg"
+  },
+  {
+    "revision": "b7c6bd09e49872bf3776df29304d8495",
+    "url": "./index.html"
+  }
+];
